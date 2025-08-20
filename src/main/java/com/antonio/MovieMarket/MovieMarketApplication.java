19f2a3adc7bc3934934adc95ac8b2a32f90222b8
Portada Luna Jorge Antonio <jorge.portada@grupopakar.com.mx>
2025-08-20 10:34:35 -0600

    Initial commit

---
+++ src/main/java/com/antonio/MovieMarket/MovieMarketApplication.java
@@ -0,0 +1,13 @@
+package com.antonio.MovieMarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieMarketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/antonio/MovieMarket/MovieMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.antonio.MovieMarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

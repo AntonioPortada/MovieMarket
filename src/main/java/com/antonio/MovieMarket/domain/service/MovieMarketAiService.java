bd80d02fa8a6b6e82c79876df038037d1c761786
Portada Luna Jorge Antonio <jorge.portada@grupopakar.com.mx>
2025-08-25 11:21:47 -0600

    refactor for Domain-oriented layered architecture : vid-5

--- src/main/java/com/antonio/MovieMarket/domain/service/MovieMarketAiService.java
+++ src/main/java/com/antonio/MovieMarket/domain/service/MovieMarketAiService.java
@@ -1,4 +1,4 @@
-package com.antonio.MovieMarket;
+package com.antonio.MovieMarket.domain.service;
 
 import dev.langchain4j.service.UserMessage;
 import dev.langchain4j.service.V;
--- src/main/java/com/antonio/MovieMarket/web/controller/HelloController.java
+++ src/main/java/com/antonio/MovieMarket/web/controller/HelloController.java
@@ -1,5 +1,6 @@
-package com.antonio.MovieMarket;
+package com.antonio.MovieMarket.web.controller;
 
+import com.antonio.MovieMarket.domain.service.MovieMarketAiService;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,7 +16,7 @@ public class HelloController {
         this.iaService = iaService;
     }
 
-    @GetMapping("/")
+    @GetMapping("/hello")
     public String hello() {
         return iaService.generateGreeting(platform);
     }

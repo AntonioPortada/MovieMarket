3b40baec0b470ed595a6fe6d1182f71b2f34d9f1
Portada Luna Jorge Antonio <jorge.portada@grupopakar.com.mx>
2025-08-25 11:00:21 -0600

    Application properties base, dev, prod (profiles) for configurations : vid-4

--- src/main/java/com/antonio/MovieMarket/HelloController.java
+++ src/main/java/com/antonio/MovieMarket/HelloController.java
@@ -1,19 +1,22 @@
 package com.antonio.MovieMarket;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
+    private final String platform;
     private final MovieMarketAiService iaService;
 
-    public HelloController(MovieMarketAiService iaService) {
+    public HelloController(@Value("${spring.application.name}") String platform, MovieMarketAiService iaService) {
+        this.platform = platform;
         this.iaService = iaService;
     }
 
     @GetMapping("/")
     public String hello() {
-        return iaService.generateGreeting();
+        return iaService.generateGreeting(platform);
     }
 }
--- src/main/java/com/antonio/MovieMarket/MovieMarketAiService.java
+++ src/main/java/com/antonio/MovieMarket/MovieMarketAiService.java
@@ -1,13 +1,15 @@
 package com.antonio.MovieMarket;
 
 import dev.langchain4j.service.UserMessage;
+import dev.langchain4j.service.V;
 import dev.langchain4j.service.spring.AiService;
 
 @AiService
 public interface MovieMarketAiService {
 
     @UserMessage("""
-            Genera un saludo de bienvenida, usa menos de 100 caracteres
+            Genera un saludo de bienvenida, usa menos de 100 caracteres, 
+            para {{platform}}
             """)
-    String generateGreeting();
+    String generateGreeting(@V("platform") String platform);
 }
